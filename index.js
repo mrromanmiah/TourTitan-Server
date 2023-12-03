d6f7681c63d59cfa0c17469edad1b7894430fb76
mrromanmiah <mrromanmiah@gmail.com>
2023-12-04 02:21:29 +0600

    admin api ready

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -29,28 +30,89 @@ async function run() {
         const wishlistCollection = client.db('tourTitan').collection('wishlist');
 
 
-        app.get('/users', async(req, res) => {
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '3h' });
+            res.send({ token });
+        })
+
+
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         })
 
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
-            const query = {email: user.email};
+            const query = { email: user.email };
             const existingUser = await userCollection.findOne(query);
             if (existingUser) {
-                return res.send ({message: 'User already exists', insertedId: null});
+                return res.send({ message: 'User already exists', insertedId: null });
             }
             const result = await userCollection.insertOne(user);
             res.send(result);
         })
 
-        app.patch('/users/admin/:id', async (req, res) => {
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
                 $set: {
                     role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        app.patch('/users/guide/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'guide'
                 }
             }
             const result = await userCollection.updateOne(filter, updatedDoc);
@@ -75,7 +137,7 @@ async function run() {
 
         app.get('/packages/:tourType', async (req, res) => {
             const tourType = req.params.tourType || '';
-            const query = {tourType: tourType};
+            const query = { tourType: tourType };
             const result = await packageCollection.find(query).toArray();
             res.send(result);
         })
@@ -88,7 +150,7 @@ async function run() {
 
         app.get('/wishlist', async (req, res) => {
             const email = req.query.email;
-            const query = {email: email}
+            const query = { email: email }
             const result = await wishlistCollection.find(query).toArray();
             res.send(result);
         })
@@ -104,7 +166,7 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result = await wishlistCollection.deleteOne(query);
             res.send(result);
-          })
+        })
 
 
 
