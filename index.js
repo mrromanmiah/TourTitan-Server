6735ad75afb6d9b3784c6ddf6864e6a3b21a2fce
mrromanmiah <mrromanmiah@gmail.com>
2023-12-05 00:03:57 +0600

    project submitted

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'https://tour-titan.web.app'
+
+    ],
+    credentials: true
+}));
 app.use(express.json());
 
 
@@ -28,6 +34,7 @@ async function run() {
         const packageCollection = client.db('tourTitan').collection('packages');
         const userCollection = client.db('tourTitan').collection('users');
         const wishlistCollection = client.db('tourTitan').collection('wishlist');
+        const bookingCollection = client.db('tourTitan').collection('bookings');
 
 
         const verifyToken = (req, res, next) => {
@@ -92,7 +99,7 @@ async function run() {
             res.send({ admin });
         })
 
-        app.get('/users/guide/:email', async (req, res) => {
+        app.get('/users/guide/:email', verifyToken, async (req, res) => {
             const email = req.params.email;
 
             if (email !== req.decoded.email) {
@@ -108,7 +115,7 @@ async function run() {
             res.send({ guide });
         })
 
-        app.post('/users', async (req, res) => {
+        app.post('/users', verifyToken, async (req, res) => {
             const user = req.body;
             const query = { email: user.email };
             const existingUser = await userCollection.findOne(query);
@@ -198,38 +205,33 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
 
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
